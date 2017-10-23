2eb273504c7a6284be1a224771b72d56ca7f89df
Peter <peterpfleiderer@gmx.de>
2017-10-23 16:08:02 +0200

    plots new linear regression

--- list_missing_data.py
+++ list_missing_data.py
@@ -2,9 +2,19 @@ import os,glob,sys
 from subprocess import Popen
 
 
+# combi=open('model-run-combinations_rcp85.txt','w')
+# models=[]
+# for file in glob.glob('blend-results.160518/rcp85-had4/*'):
+# 	model=file.split('_')[1]
+# 	run=file.split('_')[2].split('.')[0]
+# 	combi.write(model+' '+run+'\n')
+# 	models.append(model)
+# combi.close()
+# models=set(models)
+
 variable={'tas':'Amon','sic':'OImon','tos':'Omon'}
 
-combi=open('model-run-combinations.txt','r').read()
+combi=open('model-run-combinations_rcp85.txt','r').read()
 missing_tas=open('missing_tas.txt','w')
 missing_tos=open('missing_tos.txt','w')
 missing_sic=open('missing_sic.txt','w')
@@ -12,15 +22,18 @@ missing_sftof=open('missing_sftof.txt','w')
 missing={'tas':missing_tas,'sic':missing_sic,'tos':missing_tos,'sftof':missing_sftof}
 
 models=[]
-for line in combi.splitlines():
-	model=line.split(' ')[0]
-	run=line.split(' ')[1]
+for scenario in ['rcp85']:
+	for var in variable.keys():
+		missing[var].write('\n'+scenario+':\n\n')
+	for line in combi.splitlines()[0:-1]:
+		model=line.split(' ')[0]
+		run=line.split(' ')[1]
 
-	for var,group in zip(variable.keys(),variable.values())
-	if run not in glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/historical/'+group+'/'+var+'/'+model+'/*'):
-		missing[var].write(model+' '+run+'\n')
+		for var,group in zip(variable.keys(),variable.values()):
+			if run not in [mm.split('/')[-1] for mm in glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/'+scenario+'/'+group+'/'+var+'/'+model+'/*')]:
+				missing[var].write(model+' '+run+'\n')
 
-	models.append(model)
+		models.append(model)
 
 
 for model in set(models):
--- plots.py
+++ plots.py
@@ -7,6 +7,8 @@ import itertools
 import matplotlib
 from scipy import stats
 import seaborn as sns
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import summary_table
 
 def running_mean_func(xx,N):
 	if N==1:
@@ -74,6 +76,14 @@ plot_dict={
 	'had4':{'l_color':'tomato','color':red,'longname':'HadCRUT4 method\n anomalies to preindustrial'},
 	'had4_ar5':{'l_color':'cornflowerblue','color':blue,'longname':'HadCRUT4 method\n anomalies as in AR5'},
 }
+
+
+
+
+
+
+
+
 # FIG 1
 #for scenario in gmt.scenario:
 for scenario in ['rcp85']:
@@ -85,10 +95,12 @@ for scenario in ['rcp85']:
 	x=np.arange(0,5)
 
 	for method in ['tas','had4_ar5','had4']:
-		for model in gmt.model:
-			ax[0].scatter(gmt[scenario,model,'tas_ar5',:],gmt[scenario,model,method,:],color=plot_dict[method]['l_color'],marker='1',alpha=0.2)
+		# for model in gmt.model:
+		# 	ax[0].scatter(gmt[scenario,model,'tas_ar5',:],gmt[scenario,model,method,:],color=plot_dict[method]['l_color'],marker='1',alpha=0.2)
 		lr=stats.linregress(np.nanmean(gmt['rcp85',:,'tas_ar5',:],axis=0),np.nanmean(gmt['rcp85',:,method,:],axis=0))
 		ax[0].plot(x,lr[1]+x*lr[0],color=plot_dict[method]['color'],lw=2)
+
+
 		ax[0].plot([-99,-99],[-99,-99],color=plot_dict[method]['color'],lw=2,label=plot_dict[method]['longname']+'\n$\Delta T_{alt}='+str(round(lr[0],3))+'*\Delta T_{tas}'+['-','+'][int((np.sign(lr[1])+1)/2)]+str(round(abs(lr[1]),3))+'$')
 
 		y=(1.5-lr[1])/lr[0]
@@ -160,3 +172,131 @@ ax[1].set_ylabel('$\Delta T-\Delta T_{observed}~[^\circ]$')
 plt.tight_layout()
 plt.savefig('plots/GMT_ref.png')
 plt.savefig('plots/GMT_ref.pdf')
+
+
+
+
+
+
+#linear regression and estimates
+gmt_lr=da.DimArray(axes=[['rcp26','rcp45','rcp85'],['had4','had4_ar5','tas_ar5','tas'],['had4','had4_ar5','tas_ar5','tas'],['slope','slope_err','intercept','intercept_err']],dims=['scenario','x','y','out'])
+gmt_qu=da.DimArray(axes=[['rcp26','rcp45','rcp85'],['had4','had4_ar5','tas_ar5','tas'],['had4','had4_ar5','tas_ar5','tas'],[0,5,10,16.6,25,50,75,83.3,90,95,100]],dims=['scenario','x','y','out'])
+for scenario in ['rcp85']:
+	for x_method in ['had4_ar5','had4','tas','tas_ar5']:
+		plt.clf()
+		fig,axes=plt.subplots(nrows=2,ncols=2,figsize=(10,10))
+		ax=axes.flatten()
+		for y_method,pp in zip(['tas','had4_ar5','had4','tas_ar5'],range(4)):
+			x_=np.asarray(gmt[scenario,:,x_method,:]).reshape(47*2880)
+			y_=np.asarray(gmt[scenario,:,y_method,:]).reshape(47*2880)
+			idx = np.isfinite(x_) & np.isfinite(y_)
+			x,y=x_[idx],y_[idx]
+			p, V = np.polyfit(x,y, 1, cov=True)
+			gmt_lr[scenario,x_method,y_method,'slope']=p[0]
+			gmt_lr[scenario,x_method,y_method,'intercept']=p[1]
+			gmt_lr[scenario,x_method,y_method,'slope_err']=V[0,0]
+			gmt_lr[scenario,x_method,y_method,'intercept_err']=V[1,1]
+			# asdasd
+			# yy=(1.5-p[1])/(p[0])
+			# yy_up=(1.5-p[1]+V[1,1])/(p[0]-V[0,0])
+			# yy_low=(1.5-p[1]-V[1,1])/(p[0]+V[0,0])
+
+
+			tmp=np.where((y>1.45) & (y<1.55))
+			y_15=y[tmp]
+			x_15=x[tmp]
+
+			for qu in gmt_qu.out:
+				gmt_qu[scenario,x_method,y_method,qu]=np.nanpercentile(x_15,qu)
+
+
+			ax[pp].plot(x,y,marker='v',color='blue',linestyle='')
+			ax[pp].plot(x_15,y_15,marker='o',color='green',linestyle='')
+			ax[pp].plot([gmt_qu[scenario,x_method,y_method,0],gmt_qu[scenario,x_method,y_method,100]],[0.65,0.65],color='green')
+			ax[pp].fill_between([gmt_qu[scenario,x_method,y_method,25],gmt_qu[scenario,x_method,y_method,75]],[0.63,0.63],[0.67,0.67],color='green')
+			ax[pp].plot([gmt_qu[scenario,x_method,y_method,50],gmt_qu[scenario,x_method,y_method,50]],[0,1.5],color='green')
+			y50=gmt_qu[scenario,x_method,y_method,50]
+			ax[pp].text(y50,0.8,str(round(y50,3)),rotation=90,verticalalignment='center',horizontalalignment='center',backgroundcolor='white',color='green')
+
+			ax[pp].plot(x__,p[0]*x__+p[1],color='red')
+			ax[pp].plot(x__,(p[0]+V[0,0])*x__+(p[1]+V[1,1]),color='red')
+			ax[pp].plot(x__,(p[0]-V[0,0])*x__+(p[1]-V[1,1]),color='red')
+			yy=(1.5-p[1])/p[0]
+			ax[pp].plot([yy,yy],[0,1.5],color='red')
+			ax[pp].text(yy,1.1,str(round(yy,3)),rotation=90,verticalalignment='center',horizontalalignment='center',backgroundcolor='white',color='red')
+
+			ax[pp].set_xlim((1.3,1.7))
+			ax[pp].set_ylim((1.3,1.7))
+			ax[pp].set_xlabel(x_method.replace('_',' '))
+			ax[pp].set_ylabel(y_method.replace('_',' '))
+			ax[pp].plot([0,5],[0,5],'k--')
+
+		plt.savefig('plots/linear_regression_to_'+x_method+'.png')
+
+
+
+
+
+
+#
+# from scipy.optimize import curve_fit
+# plt.clf()
+# x=np.nanmean(gmt['rcp85',:,'tas_ar5',:],axis=0)
+# y=np.nanmean(gmt['rcp85',:,method,:],axis=0)
+#
+# def lf(x,a,b):
+# 	return b*x+a
+# p, cov = curve_fit(lf, x, y, [1, 1])
+# xi = np.arange(np.min(x), np.max(x), 0.01)
+# ps = np.random.multivariate_normal(p, cov, 10000)
+# ysample = np.asarray([lf(xi, *pi) for pi in ps])
+# lower = np.percentile(ysample, 2.5, axis=0)
+# upper = np.percentile(ysample, 97.5, axis=0)
+#
+# # regression estimate line
+# y_fit = lf(xi,p[0],p[1])
+#
+# # plot
+# #plt.plot(x, y, 'bo')
+# plt.plot(xi, y_fit, 'r-')
+# plt.plot(xi, lower, 'b--')
+# plt.plot(xi, upper, 'b--')
+# plt.ylim((0.61,2.5))
+# plt.xlim((0.61,2.5))
+# plt.plot([0,5],[0,5],'k--')
+# plt.savefig('plots/test.png')
+#
+#
+# plt.clf()
+# p, V = np.polyfit(x, y, 1, cov=True)
+#
+# plt.plot(x,y)
+# plt.plot(x,x*(p[0]-V[0,0])+(p[1]+V[1,1]))
+# plt.plot(x,x*(p[0]+V[0,0])+(p[1]-V[1,1]))
+# plt.plot(x,x*p[0]+p[1])
+#
+# plt.ylim((0.61,2.5))
+# plt.xlim((0.61,2.5))
+# plt.plot([0,5],[0,5],'k--')
+# plt.savefig('plots/test.png')
+#
+#
+# X = sm.add_constant(x)
+#
+# res = sm.OLS(y,X).fit()
+# st, data, ss2 = summary_table(res, alpha=0.50)
+# fittedvalues = data[:,2]
+# predict_mean_se  = data[:,3]
+# predict_mean_ci_low, predict_mean_ci_upp = data[:,4:6].T
+# predict_ci_low, predict_ci_upp = data[:,6:8].T
+# plt.plot(x, y, 'o')
+# plt.plot(x, fittedvalues, '-', lw=2)
+# plt.plot(x, predict_ci_low, 'r--', lw=2)
+# plt.plot(x, predict_ci_upp, 'r--', lw=2)
+# plt.plot(x, predict_mean_ci_low, 'r--', lw=2)
+# plt.plot(x, predict_mean_ci_upp, 'r--', lw=2)
+# plt.fill_between(x,predict_ci_low,predict_ci_upp,color=plot_dict[method]['l_color'],alpha=0.2)
+# plt.ylim((0.61,2.5))
+# plt.xlim((0.61,2.5))
+# plt.plot([0,5],[0,5],'k--')
+# plt.savefig('plots/test.png')
