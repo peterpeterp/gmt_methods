2ef75f49a25e3ff12bd80a2df3c379bcb7f92991
peterpeterp <peterpfleiderer@gmx.de>
2017-10-31 00:07:10 +0100

    numbers for Figure 2

--- plot_Figure2.py
+++ plot_Figure2.py
@@ -32,7 +32,7 @@ slr=pd.read_csv('data/slr_2100_temperature_levels_1p5_1p65.csv')
 plot_dict={
 	'gmt_sat':{'wlvl':1.4622,'name':'1.4468_ref','light_color':'orange','color':'darkorange','longname':'$\mathregular{GMT_{SAT}}$','pos':3,'lsty':'-'},
 	'gmt_ar5':{'wlvl':1.5,'name':'1.5_ref','light_color':'lawngreen','color':sns.color_palette()[1],'longname':'$\mathregular{GMT_{AR5}}$','pos':0,'lsty':'--'},
-	'gmt_millar':{'wlvl':1.65,'name':'1.6445_ref','light_color':'cornflowerblue','color':sns.color_palette()[0],'longname':'$\mathregular{GMT_{rebase-2010s}}$','pos':1,'lsty':'-'},
+	'gmt_millar':{'wlvl':1.65,'name':'1.6529_ref','light_color':'cornflowerblue','color':sns.color_palette()[0],'longname':'$\mathregular{GMT_{rebase-2010s}}$','pos':1,'lsty':'-'},
 	'gmt_bm':{'wlvl':1.7,'name':'1.6584_ref','light_color':'tomato','color':sns.color_palette()[2],'longname':'$\mathregular{GMT_{blend-mask}}$','pos':2,'lsty':'-'},
 }
 
@@ -88,12 +88,20 @@ plt.tight_layout()
 plt.savefig('plots/Figure2.png',dpi=300)
 plt.savefig('plots/Figure2.pdf')
 
-# numbers
+# number
+x=all_cmip5._distributions['global']['pdf']['xaxis']
 gmt_ar5_txx=all_cmip5._distributions['global']['pdf']['1.5_ref']
-gmt_millar_txx=all_cmip5._distributions['global']['pdf']['1._ref']
+gmt_millar_txx=all_cmip5._distributions['global']['pdf']['1.6529_ref']
 
+print sum(gmt_ar5_txx[x>1])
+print sum(gmt_millar_txx[x>1])
+print sum(gmt_ar5_txx[x>1.5])
+print sum(gmt_millar_txx[x>1.5])
 
-
+for time_slice in ['gmt_ar5','gmt_millar']:
+    y=np.array(slr[str(plot_dict[time_slice]['wlvl'])])
+    qu=np.nanpercentile(y,[5,1/6.*100,50,5/6.*100,95])
+    print time_slice,qu
 
 # #SLR check
 # plt.clf()
