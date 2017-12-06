6396fd14cc7bb81e3e6e10e29408a164730d5fb3
Peter <peterpfleiderer@gmx.de>
2017-12-06 15:39:44 +0100

    each computation individually

--- step2_check_with_old_mask.py
+++ step2_check_with_old_mask.py
@@ -4,6 +4,13 @@ from subprocess import Popen
 overwrite=True
 
 job_id=int(os.environ.get('SLURM_ARRAY_TASK_ID'))
+if job_id>=90:
+	style='xax'
+	job_id-=90
+else:
+	style='had4'
+
+
 print [fl.split('/')[-1] for fl in glob.glob('data_models/*')]
 folder=[fl.split('/')[-1] for fl in glob.glob('data_models/*')][job_id]
 print folder
@@ -16,11 +23,11 @@ if len(glob.glob('sftof/'+model+'.nc'))!=0:
 		tas='data_models/'+model+'_'+run+'/tas_'+scenario+'.nc'
 		tos='data_models/'+model+'_'+run+'/tos_'+scenario+'.nc'
 		sic='data_models/'+model+'_'+run+'/sic_'+scenario+'.nc'
-		for style in ['max']:
-			if os.path.isfile(style+'_'+scenario+'_old_mask.txt')==False or overwrite:
-				Popen('python blend-runnable/ncblendmask-nc4.py '+style+' '+tas+' '+tos+' '+sic+' '+sftof+'  data/Had_extended_old.nc > data_models/'+model+'_'+run+'/'+style+'_'+scenario+'_old_mask.txt',shell=True).wait()
-		for style in ['xax']:
+
+		if style=='xax':
 			if os.path.isfile(style+'_'+scenario+'_old_mask.txt')==False or overwrite:
 				Popen('python blend-runnable/ncblendmask-nc4.py '+style+' '+tas+' '+tos+' '+sic+' '+sftof+' > data_models/'+model+'_'+run+'/'+style+'_'+scenario+'_old_mask.txt',shell=True).wait()
-		if os.path.isfile('had4_'+scenario+'_old_mask.txt')==False or overwrite:
-			Popen('python blend-runnable/ncblendhadcrut-nc4.py '+tas+' '+tos+' '+sic+' '+sftof+'  data/CRU_extended_old.nc data/SST_extended_old.nc > data_models/'+model+'_'+run+'/had4_'+scenario+'_old_mask.txt',shell=True).wait()
+
+		if style=='had4':
+			if os.path.isfile('had4_'+scenario+'_old_mask.txt')==False or overwrite:
+				Popen('python blend-runnable/ncblendhadcrut-nc4.py '+tas+' '+tos+' '+sic+' '+sftof+'  data/CRU_extended_old.nc data/SST_extended_old.nc > data_models/'+model+'_'+run+'/had4_'+scenario+'_old_mask.txt',shell=True).wait()
