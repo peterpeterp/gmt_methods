822faee9c820504012f9a507a5ebfc695fa911aa
Peter <peterpfleiderer@gmx.de>
2017-12-06 12:40:37 +0100

    overwrite issue

--- step2_check_with_blend-runnable.py
+++ step2_check_with_blend-runnable.py
@@ -15,12 +15,12 @@ for folder in [fl.split('/')[-1] for fl in glob.glob('data_models/*')]:
 				tos='data_models/'+model+'_'+run+'/tos_'+scenario+'.nc'
 				sic='data_models/'+model+'_'+run+'/sic_'+scenario+'.nc'
 				for style in ['max']:
-					if os.path.isfile(style+'_'+scenario+'.txt')==False or overwrite:
+					if os.path.isfile(style+'_'+scenario+'__check.txt')==False or overwrite:
 						Popen('python blend-runnable/ncblendmask-nc4.py '+style+' '+tas+' '+tos+' '+sic+' '+sftof+'  blend-runnable/Had4.nc > data_models/'+model+'_'+run+'/'+style+'_'+scenario+'__check.txt',shell=True).wait()
 				for style in ['xax']:
-					if os.path.isfile(style+'_'+scenario+'.txt')==False or overwrite:
+					if os.path.isfile(style+'_'+scenario+'__check.txt')==False or overwrite:
 						Popen('python blend-runnable/ncblendmask-nc4.py '+style+' '+tas+' '+tos+' '+sic+' '+sftof+' > data_models/'+model+'_'+run+'/'+style+'_'+scenario+'__check.txt',shell=True).wait()
-				if os.path.isfile('had4_'+scenario+'.txt')==False or overwrite:
+				if os.path.isfile('had4_'+scenario+'__check.txt')==False or overwrite:
 					Popen('python blend-runnable/ncblendhadcrut-nc4.py '+tas+' '+tos+' '+sic+' '+sftof+'  blend-runnable/CRU.nc blend-runnable/SST.nc > data_models/'+model+'_'+run+'/had4_'+scenario+'__check.txt',shell=True).wait()
 
 
