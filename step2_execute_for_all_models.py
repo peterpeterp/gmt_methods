f39c3f742d0c69af0f97fcc3e6334eae8097528c
Peter <peterpfleiderer@gmx.de>
2017-12-07 13:02:42 +0100

    no data check anymore

--- step2_execute_for_all_models.py
+++ step2_execute_for_all_models.py
@@ -10,6 +10,7 @@ if job_id>=164:
 else:
 	style='had4'
 
+scenario = 'rcp85'
 
 print [fl.split('/')[-1] for fl in glob.glob('data_models/*')],len([fl.split('/')[-1] for fl in glob.glob('data_models/*')])
 folder=[fl.split('/')[-1] for fl in glob.glob('data_models/*')][job_id]
@@ -19,19 +20,17 @@ run=folder.split('_')[1]
 
 if len(glob.glob('sftof/'+model+'.nc'))!=0:
 	sftof=glob.glob('sftof/'+model+'.nc')[0]
-	scenario = 'rcp85'
-	if len(glob.glob('data_models/'+model+'_'+run+'/*'+scenario+'*.nc'))==3:
-		tas='data_models/'+model+'_'+run+'/tas_'+scenario+'.nc'
-		tos='data_models/'+model+'_'+run+'/tos_'+scenario+'.nc'
-		sic='data_models/'+model+'_'+run+'/sic_'+scenario+'.nc'
+	tas='data_models/'+model+'_'+run+'/tas_'+scenario+'.nc'
+	tos='data_models/'+model+'_'+run+'/tos_'+scenario+'.nc'
+	sic='data_models/'+model+'_'+run+'/sic_'+scenario+'.nc'
 
-		if style=='xax':
-			if os.path.isfile(style+'_'+scenario+'_old_mask.txt')==False or overwrite:
-				Popen('python blend-runnable/ncblendmask-nc4.py '+style+' '+tas+' '+tos+' '+sic+' '+sftof+' > data_models/'+model+'_'+run+'/'+style+'_'+scenario+'.txt',shell=True).wait()
+	if style=='xax':
+		if os.path.isfile(style+'_'+scenario+'_old_mask.txt')==False or overwrite:
+			Popen('python blend-runnable/ncblendmask-nc4.py '+style+' '+tas+' '+tos+' '+sic+' '+sftof+' > data_models/'+model+'_'+run+'/'+style+'_'+scenario+'.txt',shell=True).wait()
 
-		if style=='had4':
-			if os.path.isfile('had4_'+scenario+'_old_mask.txt')==False or overwrite:
-				Popen('python blend-runnable/ncblendhadcrut-nc4.py '+tas+' '+tos+' '+sic+' '+sftof+'  data/CRU_extended.nc data/SST_extended.nc > data_models/'+model+'_'+run+'/had4_'+scenario+'.txt',shell=True).wait()
+	if style=='had4':
+		if os.path.isfile('had4_'+scenario+'_old_mask.txt')==False or overwrite:
+			Popen('python blend-runnable/ncblendhadcrut-nc4.py '+tas+' '+tos+' '+sic+' '+sftof+'  data/CRU_extended.nc data/SST_extended.nc > data_models/'+model+'_'+run+'/had4_'+scenario+'.txt',shell=True).wait()
 
 
 
