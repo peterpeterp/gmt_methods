b22c3b2f0439497c6b7b0bec4eae5a8ca5db3691
Peter <peterpfleiderer@gmx.de>
2017-12-07 16:58:51 +0100

    get more sftof

--- step1_prepare_sftof.py
+++ step1_prepare_sftof.py
@@ -6,34 +6,41 @@ try:
 except:
 	job_id=1
 
-folder=[fl.split('/')[-1] for fl in glob.glob('data_models/*')][job_id]
+try:
+	os.chdir('/p/projects/tumble/carls/shared_folder/gmt')
+except:
+	os.chdir('/Users/peterpfleiderer/Documents/Projects/gmt')
+
+folder=[fl.split('/')[-1] for fl in glob.glob('data_models/*')][0]
 print folder
 model=folder.split('_')[0]
 
-if model in
 
+if model in [ff.split('_')[-1].split('.nc')[0] for ff in glob.glob('sftof/sftof_fx_*.nc')]:
+	Popen('cdo remapdis,blend-runnable/grid1x1.cdo '+file+' sftof/'+file.split('_')[2]+'.nc',shell=True).wait()
 
-for file in glob.glob('sftof/sftof_fx_*.nc'):
-        Popen('cdo remapdis,blend-runnable/grid1x1.cdo '+file+' sftof/'+file.split('_')[2]+'.nc',shell=True).wait()
+elif model in [ff.split('/')[-1] for ff in glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/historical/fx/sftlf/*')]:
+	in_file=glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/historical/fx/sftlf/'+model+'/*/*')[0]
 
+	lf=da.read_nc(in_file)['sftlf']
+	lf[lf>0]=np.nan
+	lf[lf==0]=100
+	lf[np.isfinite(lf)==False]=0
 
-for model in [file.split('/')[-1] for file in glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/historical/fx/sftlf/*')]:
+	nc_in = Dataset(in_file, "r")
+	out_file='sftof/sftof_fx_ACCESS1-0_historical_r0i0p0_from_sftlft.nc'
+	os.system('rm '+out_file)
+	nc_out=Dataset(out_file,"w")
+	for dname, the_dim in nc_in.dimensions.iteritems():
+		nc_out.createDimension(dname, len(the_dim) if not the_dim.isunlimited() else None)
+	for v_name, varin in nc_in.variables.iteritems():
+		if v_name=='sftlf':
+			v_name='sftof'
+		outVar = nc_out.createVariable(v_name, varin.datatype, varin.dimensions)
+		outVar.setncatts({k: varin.getncattr(k) for k in varin.ncattrs()})
+		if v_name=='sftof':	outVar[:] = lf.values
+		else:	outVar[:] = varin[:]
+	nc_out.close()
+	nc_in.close()
 
-	Popen('mkdir '+model+'_'+run, shell=True).wait()
-	os.chdir(model+'_'+run+'/')
-
-
-	command='cdo -a mergetime '
-	hist_files=glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/historical/'+group+'/'+var+'/'+model+'/'+run+'/*')
-	scenario_files=glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/'+scenario+'/'+group+'/'+var+'/'+model+'/'+run+'/*')
-	if len(scenario_files)!=0 and (os.path.isfile(var+'_'+scenario+'.nc')==False or overwrite):
-		for file in hist_files+scenario_files:
-			print file
-			command+=file+' '
-		Popen(command+'tmp_m_'+var+'.nc',shell=True).wait()
-
-		Popen('cdo selyear,1850/2100 tmp_m_'+var+'.nc tmp_s_'+var+'.nc',shell=True).wait()
-		Popen('cdo remapdis,../../blend-runnable/grid1x1.cdo tmp_s_'+var+'.nc '+var+'_'+scenario+'.nc',shell=True).wait()
-		Popen('rm tmp_s_'+var+'.nc tmp_m_'+var+'.nc',shell=True).wait()
-
-	os.chdir('../')
+	Popen('cdo remapdis,blend-runnable/grid1x1.cdo '+out_file+' sftof/'+model+'.nc',shell=True).wait()
