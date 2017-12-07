f7fe302a23faf9394df1db81f7cb40f3e8d0c655
Peter <peterpfleiderer@gmx.de>
2017-12-07 15:23:50 +0100

    done

--- step1_prepare_data.py
+++ step1_prepare_data.py
@@ -33,4 +33,4 @@ for scenario,selyear in zip(['rcp85','historical'],['2006/2100','1850/2005']):
 
 			Popen('cdo selyear,'+selyear+' tmp_m_'+var+'.nc tmp_s_'+var+'.nc',shell=True).wait()
 			Popen('cdo remapdis,../../blend-runnable/grid1x1.cdo tmp_s_'+var+'.nc '+var+'_'+scenario+'.nc',shell=True).wait()
-			#Popen('rm tmp_s_'+var+'.nc tmp_m_'+var+'.nc',shell=True).wait()
+			Popen('rm tmp_s_'+var+'.nc tmp_m_'+var+'.nc',shell=True).wait()
