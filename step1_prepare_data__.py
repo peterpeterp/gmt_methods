d7b822bd52f60d6e496d2ddf4126ca203f806cff
Peter <peterpfleiderer@gmx.de>
2017-12-08 15:56:21 +0100

    simple CDO fix

--- step1_prepare_data__.py
+++ step1_prepare_data__.py
@@ -40,17 +40,16 @@ os.system('export SKIP_SAME_TIME=1')
 
 variable={'tas':'Amon','sic':'OImon','tos':'Omon'}
 
-def normal_procedure(model,run,scenario,group,var,overwrite):
+def normal_procedure(model,run,scenario,selyear,group,var,overwrite):
 	command='cdo -a mergetime '
-	hist_files=glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/historical/'+group+'/'+var+'/'+model+'/'+run+'/*')
 	scenario_files=glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/'+scenario+'/'+group+'/'+var+'/'+model+'/'+run+'/*')
 	if len(scenario_files)!=0 and (os.path.isfile(var+'_'+scenario+'.nc')==False or overwrite):
-		for file_name in scenario_files+hist_files:
+		for file_name in scenario_files:
 			print file_name
 			command+=file_name+' '
 		Popen(command+'tmp_m_'+var+'.nc',shell=True).wait()
 
-		Popen('cdo selyear,1850/2099 tmp_m_'+var+'.nc tmp_s_'+var+'.nc',shell=True).wait()
+		Popen('cdo selyear,'+selyear+' tmp_m_'+var+'.nc tmp_s_'+var+'.nc',shell=True).wait()
 		Popen('cdo -O remapdis,../../blend-runnable/grid1x1.cdo tmp_s_'+var+'.nc '+var+'_'+scenario+'.nc',shell=True).wait()
 		Popen('rm tmp_s_'+var+'.nc tmp_m_'+var+'.nc',shell=True).wait()
 	if len(scenario_files)==0:
@@ -58,13 +57,70 @@ def normal_procedure(model,run,scenario,group,var,overwrite):
 		info.write('bla')
 		info.close()
 
-for scenario in ['rcp85']:
-	for var,group in zip(variable.keys(),variable.values()):
-		print scenario,var,group
-		print model,run
-		normal_procedure(model,run,scenario,group,var,overwrite)
+if model_run=='EC-EARTH_r6i1p1':
+	for scenario,selyear in zip(['rcp85','historical'],['2006/2100','1850/2005']):
+		for var,group in zip(variable.keys(),variable.values()):
+			if scenario=='rcp85' and var=='tas':
+				print scenario,var,group
+				print model,run
 
-asdas
+				command='cdo -a mergetime '
+				scenario_files=glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/'+scenario+'/'+group+'/'+var+'/'+model+'/'+run+'/*')
+				for file_name in scenario_files:
+					if '205101-210012.nc' not in file_name.split('_') and '200601-205012.nc' not in file_name.split('_'):
+						print file_name
+						command+=file_name+' '
+				Popen(command+'tmp_m_'+var+'.nc',shell=True).wait()
+				Popen('cdo selyear,'+selyear+' tmp_m_'+var+'.nc tmp_s_'+var+'.nc',shell=True).wait()
+				Popen('cdo -O remapdis,../../blend-runnable/grid1x1.cdo tmp_s_'+var+'.nc '+var+'_'+scenario+'.nc',shell=True).wait()
+				Popen('rm tmp_s_'+var+'.nc tmp_m_'+var+'.nc',shell=True).wait()
+			else:
+				normal_procedure(model,run,scenario,selyear,group,var,overwrite)
+
+
+elif model=='HadGEM2-AO':
+	for scenario,selyear in zip(['rcp85','historical'],['2006/2100','1850/2005']):
+		for var,group in zip(variable.keys(),variable.values()):
+			if scenario=='rcp85' and var=='tas':
+				print scenario,var,group
+				print model,run
+
+				command='cdo -a mergetime '
+				scenario_files=glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/'+scenario+'/'+group+'/'+var+'/'+model+'/'+run+'/*')
+				for file_name in scenario_files:
+					if '200601-209912.nc' not in file_name.split('_'):
+						print file_name
+						command+=file_name+' '
+				Popen(command+'tmp_m_'+var+'.nc',shell=True).wait()
+				Popen('cdo selyear,'+selyear+' tmp_m_'+var+'.nc tmp_s_'+var+'.nc',shell=True).wait()
+				Popen('cdo -O remapdis,../../blend-runnable/grid1x1.cdo tmp_s_'+var+'.nc '+var+'_'+scenario+'.nc',shell=True).wait()
+				Popen('rm tmp_s_'+var+'.nc tmp_m_'+var+'.nc',shell=True).wait()
+			else:
+				normal_procedure(model,run,scenario,selyear,group,var,overwrite)
+
+# elif 'Had' in model.split('GEM'):
+# 	for var,group in zip(variable.keys(),variable.values()):
+# 		hist_files=glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/historical/'+group+'/'+var+'/'+model+'/'+run+'/*')
+# 		if '200512.nc' in [ff.split('-')[-1] for ff in hist_files]:
+# 			print '2005/12 in hist'
+# 			for scenario,selyear in zip(['rcp85','historical'],['2006/2100','1850/2005']):
+# 				normal_procedure(model,run,scenario,selyear,group,var,overwrite)
+# 		if '200512.nc' not in [ff.split('-')[-1] for ff in hist_files]:
+# 			print '2005/12 in future'
+# 			for scenario,selyear in zip(['rcp85','historical'],['2005/2100','1850/2005']):
+# 				normal_procedure(model,run,scenario,selyear,group,var,overwrite)
+#
+
+
+# all clean files
+else:
+	for scenario,selyear in zip(['rcp85','historical'],['2006/2100','1850/2005']):
+		for var,group in zip(variable.keys(),variable.values()):
+			print scenario,var,group
+			print model,run
+			normal_procedure(model,run,scenario,selyear,group,var,overwrite)
+
+
 
 # ++++++++++++++++++++++++++++++
 # + merge scenarios
