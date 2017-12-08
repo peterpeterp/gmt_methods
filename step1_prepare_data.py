41b8e2402e9e715f812e2e5f9ab3280360d46d47
Peter <peterpfleiderer@gmx.de>
2017-12-08 10:20:40 +0100

    one prepare data file with individual fixes

--- step1_prepare_data.py
+++ step1_prepare_data.py
@@ -18,21 +18,81 @@ os.chdir('data_models/'+model+'_'+run+'/')
 
 variable={'tas':'Amon','sic':'OImon','tos':'Omon'}
 
-for scenario,selyear in zip(['rcp85','historical'],['2006/2100','1850/2005']):
-	for var,group in zip(variable.keys(),variable.values()):
-		print scenario,var,group
-		print model,run
 
-		command='cdo -a mergetime '
-		scenario_files=glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/'+scenario+'/'+group+'/'+var+'/'+model+'/'+run+'/*')
-		if len(scenario_files)!=0 and (os.path.isfile(var+'_'+scenario+'.nc')==False or overwrite):
-			for file_name in scenario_files:
-				print file_name
-				command+=file_name+' '
-			Popen(command+'tmp_m_'+var+'.nc',shell=True).wait()
 
-			if model.split('d')[0]=='Ha' and scenario=='rcp85':
-				selyear='2005/2100'
-			Popen('cdo selyear,'+selyear+' tmp_m_'+var+'.nc tmp_s_'+var+'.nc',shell=True).wait()
-			Popen('cdo -O remapdis,../../blend-runnable/grid1x1.cdo tmp_s_'+var+'.nc '+var+'_'+scenario+'.nc',shell=True).wait()
-			Popen('rm tmp_s_'+var+'.nc tmp_m_'+var+'.nc',shell=True).wait()
+if model_run=='EC-EARTH_r6i1p1'
+	for scenario,selyear in zip(['rcp85','historical'],['2006/2100','1850/2005']):
+		for var,group in zip(variable.keys(),variable.values()):
+			if scenario=='rcp85' and var=='tas':
+				print scenario,var,group
+				print model,run
+
+				command='cdo -a mergetime '
+				scenario_files=glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/'+scenario+'/'+group+'/'+var+'/'+model+'/'+run+'/*')
+				for file_name in scenario_files:
+					if '205101-210012.nc' not in file_name.split('_') and '200601-205012.nc' not in file_name.split('_'):
+						print file_name
+						command+=file_name+' '
+				Popen(command+'tmp_m_'+var+'.nc',shell=True).wait()
+				Popen('cdo selyear,'+selyear+' tmp_m_'+var+'.nc tmp_s_'+var+'.nc',shell=True).wait()
+				Popen('cdo -O remapdis,../../blend-runnable/grid1x1.cdo tmp_s_'+var+'.nc '+var+'_'+scenario+'.nc',shell=True).wait()
+				Popen('rm tmp_s_'+var+'.nc tmp_m_'+var+'.nc',shell=True).wait()
+
+
+elif model=='HadGEM2-AO':
+	for scenario,selyear in zip(['rcp85','historical'],['2006/2100','1850/2005']):
+		for var,group in zip(variable.keys(),variable.values()):
+			if scenario=='rcp85' and var=='tas':
+				print scenario,var,group
+				print model,run
+
+				command='cdo -a mergetime '
+				scenario_files=glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/'+scenario+'/'+group+'/'+var+'/'+model+'/'+run+'/*')
+				for file_name in scenario_files:
+					if '200601-209912.nc' not in file_name.split('_'):
+						print file_name
+						command+=file_name+' '
+				Popen(command+'tmp_m_'+var+'.nc',shell=True).wait()
+				Popen('cdo selyear,'+selyear+' tmp_m_'+var+'.nc tmp_s_'+var+'.nc',shell=True).wait()
+				Popen('cdo -O remapdis,../../blend-runnable/grid1x1.cdo tmp_s_'+var+'.nc '+var+'_'+scenario+'.nc',shell=True).wait()
+				Popen('rm tmp_s_'+var+'.nc tmp_m_'+var+'.nc',shell=True).wait()
+
+elif 'Had' in model.split('GEM'):
+	for scenario,selyear in zip(['rcp85','historical'],['2005/2100','1850/2005']):
+		for var,group in zip(variable.keys(),variable.values()):
+			if scenario=='rcp85' and var=='tas':
+				print scenario,var,group
+				print model,run
+
+				command='cdo -a mergetime '
+				scenario_files=glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/'+scenario+'/'+group+'/'+var+'/'+model+'/'+run+'/*')
+				for file_name in scenario_files:
+					print file_name
+					command+=file_name+' '
+				Popen(command+'tmp_m_'+var+'.nc',shell=True).wait()
+				Popen('cdo selyear,'+selyear+' tmp_m_'+var+'.nc tmp_s_'+var+'.nc',shell=True).wait()
+				Popen('cdo -O remapdis,../../blend-runnable/grid1x1.cdo tmp_s_'+var+'.nc '+var+'_'+scenario+'.nc',shell=True).wait()
+				Popen('rm tmp_s_'+var+'.nc tmp_m_'+var+'.nc',shell=True).wait()
+
+
+
+# all clean files
+else:
+	for scenario,selyear in zip(['rcp85','historical'],['2006/2100','1850/2005']):
+		for var,group in zip(variable.keys(),variable.values()):
+			print scenario,var,group
+			print model,run
+
+			command='cdo -a mergetime '
+			scenario_files=glob.glob('/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/'+scenario+'/'+group+'/'+var+'/'+model+'/'+run+'/*')
+			if len(scenario_files)!=0 and (os.path.isfile(var+'_'+scenario+'.nc')==False or overwrite):
+				for file_name in scenario_files:
+					print file_name
+					command+=file_name+' '
+				Popen(command+'tmp_m_'+var+'.nc',shell=True).wait()
+
+				if model.split('d')[0]=='Ha' and scenario=='rcp85':
+					selyear='2005/2100'
+				Popen('cdo selyear,'+selyear+' tmp_m_'+var+'.nc tmp_s_'+var+'.nc',shell=True).wait()
+				Popen('cdo -O remapdis,../../blend-runnable/grid1x1.cdo tmp_s_'+var+'.nc '+var+'_'+scenario+'.nc',shell=True).wait()
+				Popen('rm tmp_s_'+var+'.nc tmp_m_'+var+'.nc',shell=True).wait()
