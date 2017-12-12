1273f9294c9fdb4a9ae43202ab409d780d5197cc
Peter <peterpfleiderer@gmx.de>
2017-12-12 12:26:14 +0100

    read rempa styles

--- step3_collect_all_runs.py
+++ step3_collect_all_runs.py
@@ -21,19 +21,18 @@ styles=['xax','had4']
 
 variables=['air','gmt']
 
+for sftof_style in ['_remapdis','_remapnn']:
+	tmp_example=pd.read_table('data_models/ACCESS1-0_r1i1p1/had4_rcp85.txt',sep=' ',header=None)
+	gmt=da.DimArray(axes=[styles,['rcp85'],model_runs,variables,np.array(tmp_example[0])],dims=['style','scenario','model_run','variable','time'])
 
-tmp_example=pd.read_table('data_models/ACCESS1-0_r1i1p1/had4_rcp85.txt',sep=' ',header=None)
-gmt=da.DimArray(axes=[styles,['rcp85'],model_runs,variables,np.array(tmp_example[0])],dims=['style','scenario','model_run','variable','time'])
-
-for style in gmt.style:
-	print style
-	for scenario in gmt.scenario:
-		print scenario
-		for model_run in model_runs:
-			if len(glob.glob('data_models/'+model_run+'/*'+scenario+'*.txt'))!=0:
+	for style in gmt.style:
+		print style
+		for scenario in gmt.scenario:
+			print scenario
+			for model_run in model_runs:
 				#tmp=pd.read_table('data_models/'+model+'_'+run+'/'+style+'_'+scenario+'.txt',sep=' ',header=None)
 				try:
-					tmp=pd.read_table('data_models/'+model_run+'/'+style+'_'+scenario+'.txt',sep=' ',header=None)
+					tmp=pd.read_table('data_models/'+model_run+'/'+style+'_'+scenario+sftof_style+'.txt',sep=' ',header=None)
 					tmp.columns=['time','air','gmt','diff']
 					time_ax=np.array(tmp['time'])
 					useful_years=time_ax[(time_ax>1850) & (time_ax<2100)]
@@ -43,5 +42,5 @@ for style in gmt.style:
 					pass
 
 
-ds=da.Dataset({'gmt':gmt})
-ds.write_nc('data/gmt_all.nc', mode='w')
+	ds=da.Dataset({'gmt':gmt})
+	ds.write_nc('data/gmt_all'+sftof_style+'.nc', mode='w')
