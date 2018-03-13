fcedb8bf04b805d5f6f7e07083253f009f578e45
Peter <peterpfleiderer@gmx.de>
2018-03-13 14:10:12 +0000

    deleting temporary files

--- step0_prepare_data.py
+++ step0_prepare_data.py
@@ -90,7 +90,7 @@ def normal_procedure(model,run,scenario,group,var,overwrite):
 			Popen('cdo selyear,1850/2099 tmp_m_'+var+'.nc tmp_s_'+var+'.nc',shell=True).wait()
 
 		Popen('cdo -O remapdis,../../blend-runnable/grid1x1.cdo tmp_s_'+var+'.nc '+var+'_'+scenario+'.nc',shell=True).wait()
-		#Popen('rm tmp_s_'+var+'.nc tmp_m_'+var+'.nc',shell=True).wait()
+		Popen('rm tmp_s_'+var+'.nc tmp_m_'+var+'.nc',shell=True).wait()
 	if len(scenario_files)==0:
 		info=open('delete_here','w')
 		info.write('bla')
