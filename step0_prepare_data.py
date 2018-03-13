4feb0ed6e3aa62421e7310b488cb731ee5bea247
Peter <peterpfleiderer@gmx.de>
2018-03-13 11:37:47 +0000

    cdo version trouble

--- step0_prepare_data.py
+++ step0_prepare_data.py
@@ -46,7 +46,8 @@ except:
 	else:
 		var_names=['tas','sic','tos']
 
-
+# there seems to be some issue with cdo/1.8.0 and this script
+os.system('module load cdo/1.7.0')
 
 
 #Popen('mkdir data_models/'+model+'_'+run, shell=True).wait()
@@ -59,6 +60,7 @@ os.system('export SKIP_SAME_TIME=1')
 # ++++++++++++++++++++++++++++++
 
 variable={'tas':'Amon','sic':'OImon','tos':'Omon'}
+
 
 def normal_procedure(model,run,scenario,group,var,overwrite):
 	command='cdo -a mergetime '
@@ -77,7 +79,7 @@ def normal_procedure(model,run,scenario,group,var,overwrite):
 			# /!\ this condition might become a problem when there are other 0 appearing in the output
 			if len(cdoinfo.split('\n')[1].split(' 0 '))==3:
 				# change 273.15 to missing
-				Popen('cdo selyear,1850/2099 -setmissval,273.15 tmp_m_'+var+'.nc tmp_s_'+var+'.nc',shell=True).wait()
+				Popen('cdo selyear,1850/2099 -m 1.0e20 setrtomiss,273.14,273.16 tmp_m_'+var+'.nc tmp_s_'+var+'.nc',shell=True).wait()
 			else:
 				Popen('cdo selyear,1850/2099 tmp_m_'+var+'.nc tmp_s_'+var+'.nc',shell=True).wait()
 
