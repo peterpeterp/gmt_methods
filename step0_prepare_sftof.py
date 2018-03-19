f6b2826ec5365c22aac538a7aecd82dddfa2be01
Peter <peterpfleiderer@gmx.de>
2018-03-19 12:22:01 +0000

    there is an issue with converting sftlft into sftof droping this workaround

--- step0_prepare_sftof.py
+++ step0_prepare_sftof.py
@@ -12,37 +12,44 @@ except:
 
 missing_sftof=open('missing_sftof.txt','w')
 
+for file_name in glob.glob('sftof_raw/sftof_fx_*_historical_r0i0p0.nc'):
+	model=file_name.split('_')[-3]
+	Popen("cdo remapdis,blend-runnable/grid1x1.cdo "+file_name+" sftof/"+model+".nc",shell=True).wait()
 
+
+missing_sftof=open('missing_sftof.txt','w')
 for folder in [fl.split('/')[-1] for fl in glob.glob('data_models/*')]:
-	print folder
 	model=folder.split('_')[0]
-	run=folder.split('_')[1]
-	model_run=model+'_'+run
-
-	rel_path='data_models/'+model_run+'/'
-
-	if model in [ff.split('_')[-3] for ff in glob.glob('sftof/sftof_fx_*_historical_r0i0p0.nc')]:
-		Popen('cp sftof/sftof_fx_'+model+'_historical_r0i0p0.nc '+rel_path+'sftof_raw.nc',shell=True).wait()
-
-	elif model in [ff.split('_')[-3] for ff in glob.glob('sftof/sftof_*_from_sftlft.nc')]:
-		Popen('cp sftof/sftof_'+model+'_from_sftlft.nc '+rel_path+'sftof_raw.nc',shell=True).wait()
-
-	else:
+	if model not in [ff.split('/')[-1] for ff in glob.glob('sftof/*')]:
 		missing_sftof.write(model_run+'\n')
-
-	if os.path.isfile(rel_path+'sftof_raw.nc'):
-
-		Popen("cdo -expr,'sftof=(sftof>0.0)?1.0:sftof;' "+rel_path+"sftof_raw.nc "+rel_path+"sftof_01.nc",shell=True).wait()
-		Popen("cdo -setmissval,0 "+rel_path+"sftof_01.nc "+rel_path+"sftof_NaN1.nc",shell=True).wait()
-
-		Popen("cdo remapdis,blend-runnable/grid1x1.cdo "+rel_path+"sftof_01.nc "+rel_path+"sftof_01_1x1.nc",shell=True).wait()
-		Popen("cdo remapdis,blend-runnable/grid1x1.cdo "+rel_path+"sftof_NaN1.nc "+rel_path+"sftof_NaN1_1x1.nc",shell=True).wait()
-
-
-
-
 missing_sftof.close()
 
+# for folder in [fl.split('/')[-1] for fl in glob.glob('data_models/*')]:
+# 	print folder
+# 	model=folder.split('_')[0]
+# 	run=folder.split('_')[1]
+# 	model_run=model+'_'+run
+#
+# 	rel_path='data_models/'+model_run+'/'
+#
+# 	if model in [ff.split('_')[-3] for ff in glob.glob('sftof/sftof_fx_*_historical_r0i0p0.nc')]:
+# 		Popen('cp sftof/sftof_fx_'+model+'_historical_r0i0p0.nc '+rel_path+'sftof_raw.nc',shell=True).wait()
+#
+# 	elif model in [ff.split('_')[-3] for ff in glob.glob('sftof/sftof_*_from_sftlft.nc')]:
+# 		Popen('cp sftof/sftof_'+model+'_from_sftlft.nc '+rel_path+'sftof_raw.nc',shell=True).wait()
+#
+# 	else:
+# 		missing_sftof.write(model_run+'\n')
+#
+# 	if os.path.isfile(rel_path+'sftof_raw.nc'):
+#
+# 		Popen("cdo -expr,'sftof=(sftof>0.0)?1.0:sftof;' "+rel_path+"sftof_raw.nc "+rel_path+"sftof_01.nc",shell=True).wait()
+# 		Popen("cdo -setmissval,0 "+rel_path+"sftof_01.nc "+rel_path+"sftof_NaN1.nc",shell=True).wait()
+#
+# 		Popen("cdo remapdis,blend-runnable/grid1x1.cdo "+rel_path+"sftof_01.nc "+rel_path+"sftof_01_1x1.nc",shell=True).wait()
+# 		Popen("cdo remapdis,blend-runnable/grid1x1.cdo "+rel_path+"sftof_NaN1.nc "+rel_path+"sftof_NaN1_1x1.nc",shell=True).wait()
+#
+# missing_sftof.close()
 #
 # for file_name in glob.glob('sftof/sftof_fx_*_historical_r0i0p0.nc'):
 # 	model = file_name.split('_')[-2]
